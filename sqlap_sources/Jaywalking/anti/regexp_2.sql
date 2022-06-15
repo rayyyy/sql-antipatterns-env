205480b9bf85e54b6324c9b84484917e42263763
rainiero <nraavy@gmail.com>
2022-06-15 20:06:06 +0900

    mysql 8用に直した

--- sqlap_sources/Jaywalking/anti/regexp_2.sql
+++ sqlap_sources/Jaywalking/anti/regexp_2.sql
@@ -1,3 +1,3 @@
 SELECT * FROM Products AS p INNER JOIN Accounts AS a
-    ON p.account_id REGEXP '[[:<:]]' || a.account_id || '[[:>:]]'
+    ON p.account_id REGEXP '\\b' || a.account_id || '\\b'
 WHERE p.product_id = 123;
